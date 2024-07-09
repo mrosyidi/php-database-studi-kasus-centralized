c15c1d025120355839276348f21999993fb0007d
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-07-09 13:00:52 +0700

    update TodolistService.php menampilkan todolist

--- Service/TodolistService.php
+++ Service/TodolistService.php
@@ -2,10 +2,44 @@
 
   namespace Service
   {
+    use Repository\TodolistRepository;
+
     interface TodolistService
     {
       public function showTodolist(): void;
       public function addTodolist(string $todo): void;
       public function removeTodo(int $number): void;
+    }
+
+    class TodolistServiceImpl implements TodolistService
+    {
+      private TodolistRepository $todolistRepository;
+
+      public function __construct(TodolistRepository $todolistRepository)
+      {
+        $this->todolistRepository = $todolistRepository;
+      }
+
+      public function showTodolist(): void
+      {
+        echo "TODOLIST" . PHP_EOL;
+
+        $todolist = $this->todolistRepository->findAll();
+        foreach($todolist as $number => $todo)
+        {
+          $number = $number + 1;
+          echo "$number. " . $todo->getTodo() . PHP_EOL;
+        }
+      }
+
+      public function addTodolist(string $todo): void
+      {
+
+      }
+
+      public function removeTodo(int $number): void
+      {
+
+      }
     }
   }
