ba4abc55e7a9cdc4f85358c58671dfac448bc0c8
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-07-09 10:15:40 +0700

    update TodolistRepository.php menampilkan todolist

--- Repository/TodolistRepository.php
+++ Repository/TodolistRepository.php
@@ -10,4 +10,43 @@
       public function remove(int $number): bool;
       public function findAll(): array;
     }
+
+    class TodolistRepositoryImpl implements TodolistRepository
+    {
+      public array $todolist = array();
+      private \PDO $connection;
+
+      public function __construct(\PDO $connection)
+      {
+        $this->connection = $connection;
+      }
+
+      public function save(Todolist $todolist): void
+      {
+
+      }
+
+      public function remove(int $number): bool
+      {
+
+      }
+
+      public function findAll(): array
+      {
+        $sql = "SELECT id, todo FROM todolist";
+        $statement = $this->connection->prepare($sql);
+        $statement->execute();
+        $todolist = [];
+
+        foreach($statement as $row)
+        {
+          $todo = new Todolist();
+          $todo->setId($row['id']);
+          $todo->setTodo($row['todo']);
+          $todolist[] = $todo;
+        }
+
+        return $todolist;
+      }
+    }
   }
