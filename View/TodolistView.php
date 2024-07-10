2a2125e128b3fea08b4b6eed519fc87a9757075e
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-07-10 18:03:15 +0700

    update TodolistView.php menghapus todolist

--- View/TodolistView.php
+++ View/TodolistView.php
@@ -58,5 +58,20 @@
           $this->todolistService->addTodolist($todo);
         }
       }
+
+      public function removeTodolist(): void
+      {
+        echo "MENGHAPUS TODOLIST" . PHP_EOL;
+
+        $number = InputHelper::input("Nomor (x untuk batalkan)");
+
+        if($number == "x")
+        {
+          echo "Batal menghapus todo" . PHP_EOL;
+        }else
+        {
+          $this->todolistService->removeTodolist($number);
+        }
+      }
     }
   }
